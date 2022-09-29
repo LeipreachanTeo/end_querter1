6e5a78ee91d49cb0863de441f89033cd8a1ef428
LeipreachanTeo <pokiboom@mail.ru>
2022-09-29 16:30:08 +0700

    add task

---
+++ task/task.cs
@@ -0,0 +1,28 @@
+﻿string [] array = new string[5] {"Hello","55", "2", "54", "Print"};
+string [] second_array = new string[array.Length];
+
+void NewArrayLess4Char(string[] array, string[] array2)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArrayLess4Char(array, second_array);
+PrintArray(second_array);
+
